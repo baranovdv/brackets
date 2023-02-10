2a2321d9e1d5b079e9e43aa248adff9c300a16c5
baranovdv <baranov.d.v.1988@gmail.com>
2023-02-10 12:38:25 +0700

    -feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+ 
+  //config2obj
+  // bracketsConfig = [['(', ')'], ['[', ']']]
+
+  let bracketsConfigObj = {}
+  let stack = []
+  let counter = 0
+  let arr = []
+
+  bracketsConfigObj = bracketsConfig.reduce((acc,item) => {
+    acc[item[0]] = item[1] 
+    return acc  
+  },{})
+
+
+  for(i of str){
+    console.log(i)
+
+    if(Object.keys(bracketsConfigObj).includes(i)){
+      
+      if(stack.includes(i) && bracketsConfigObj[i] == i){
+        
+          if(stack.pop() !== i){
+            return false
+          } else {
+            continue
+          }
+        
+      }
+
+      // console.log(stack)
+      stack.push(i)
+      
+    }
+
+
+    if(Object.values(bracketsConfigObj).includes(i) && bracketsConfigObj[i] !== i){
+
+      if(bracketsConfigObj[stack.pop()] !== i){
+        return false
+      }
+    }
+
+  }
+
+  if(stack.length > 0) {
+    return false
+  }
+
+  return true
+  // console.log(bracketsConfigObj)
+
 }
+
+//  console.log(module.exports('|(|)',[['(', ')'], ['|', '|']]))
